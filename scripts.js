afb59f2e44d5462e4bcfe805c5e2dd41c41a7cdc
Kumo-chan <patrick.furrer@heig-vd.ch>
2023-10-10 21:32:19 +0200

    Pass the function as a parameter so that it can be used somewhere else

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-document.getElementById("terminate").addEventListener("click", function () {
-    /*
+document.getElementById("terminate").addEventListener("click", setBackGroundColorToRandomColor);
+/*
       Generation of random color is directly taken from this site:
       https://css-tricks.com/snippets/javascript/random-hex-color/
      */
+function setBackGroundColorToRandomColor() {
     let randomColor = Math.floor(Math.random() * 16777215).toString(16);
     document.body.style.backgroundColor = "#" + randomColor;
-});
+}
